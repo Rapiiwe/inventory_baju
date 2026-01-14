86407c1321bab1c48aab15fac2d409b8472157f8
Rio Ivano <101827865+rioivano@users.noreply.github.com>
2026-01-14 23:33:21 +0700

    update ganti nama

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = 'inventori_baju';
+$dbname = 'inventory_baju';
 
 // Koneksi ke database
 $conn = new mysqli($host, $username, $password, $dbname);
